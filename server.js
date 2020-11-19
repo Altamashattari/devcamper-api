66a403b734cee266fe0a2df11b7089d5ead12dce
Altamash Ali <maa0027@MacBook-Pro.local>
2020-11-19 10:20:00 +0530

    Database Connection Setup

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
-// Route Files
+const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
 
-// Load env vars
-dotenv.config({path: './config/config.env'});
 
+// Load env vars
+dotenv.config({
+    path: './config/config.env'
+});
+
+// connect to Database
+connectDB();
+
+// Route Files
 const app = express();
 
 // DEV Logging middleware
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
 // Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
-
-
 const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} environment on PORT ${PORT}`.yellow.bold));
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} environment on PORT ${PORT}`));
+
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`.red);
+    server && server.close(() => process.exit(1));
+});
