2dbc9831b6ef4752d3e17b4cb6ec387269dd5f66
Md Sultan Altamash Ali <mdsultan.ali@walmartlabs.com>
2021-03-04 15:50:38 +0530

    Calculating avg of courses tuitions in a bootcamp

--- models/Course.js
+++ models/Course.js
@@ -37,5 +37,38 @@ const CourseSchema = new mongoose.Schema({
         required: true,
     }
 });
+// Static method to get average of course tuitions in a bootcamp
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log('Calculating average cost'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: { 
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' }
+             }
+        }
+    ]);
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: obj && obj[0] ? Math.ceil(obj[0].averageCost/10)*10 : 0
+        })
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// call getAverageCost after save
+CourseSchema.post('save', function() {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+// call getAverageCost before remove
+CourseSchema.pre('remove', function() {
+    this.constructor.getAverageCost(this.bootcamp);
+});
 
 module.exports = mongoose.model('Course', CourseSchema);
